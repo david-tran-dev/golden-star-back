5a9de100265a87f195404f41d3c9712f7fcc2dff
David TRAN <david.tran.devweb@gmail.com>
2022-04-26 21:07:37 +0200

    upgrade admin panel version

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '8916bd96e3459debc6f9260dbbdd862d'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  }
 });
